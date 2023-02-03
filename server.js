30c64aaf4d6075c19dcb05d9e3310ba2a46a2da1
Geraldo Vicente <geraldovicente987@gmail.com>
2023-02-03 09:13:36 -0300

    Defining file that will handle server information

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const app = require("./app");
+const app = require('./app');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env' });
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(
+    process.env.DATABASE_URL.replace('password', process.env.DATABASE_PASSWORD),
+);
 
 app.listen(3000, () => {
-  console.log("Server started on port 3000");
+    console.log('Server started on port 3000');
 });
