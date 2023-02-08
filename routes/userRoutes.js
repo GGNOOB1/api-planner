97e86094148d742c8cf985b7ea675f8f766387e0
Geraldo Vicente <geraldovicente987@gmail.com>
2023-02-07 22:43:29 -0300

    Defining routes and handler functions for users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const validateUser = require('../validations/userValidators');
 
 const userController = require('../controllers/userController');
 
-router.post('/signUp', userController.signUp);
-router.post('/signIn', userController.signIn);
-router.put('/:id', userController.updateUser);
+router.post('/signUp', validateUser.validateUser, userController.signUp);
+router.post('/signIn', validateUser.validateLogin, userController.signIn);
+router.put('/:id', validateUser.validateUser, userController.updateUser);
 
 module.exports = router;
